0320a4739bb8f09d6eabeba01ad783d08d78d028
Zviad Mikeladze <zviadipf@gmail.com>
2022-10-17 20:15:21 +0400

    nav-pages

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,26 @@
- 
+import MeetupList from "../components/meetups/MeetupList.js";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    addres: "Some addres 5 , 1245 City",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    addres: "Some addres 55 , 126845 City A",
+    description: "This is a Second meetup",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;

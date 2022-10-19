cbbcab49086b896e842b0d5cad5ec89da11b82a7
Zviad Mikeladze <zviadipf@gmail.com>
2022-10-19 15:45:52 +0400

    page url id

---
+++ components/meetups/MeetupDetail.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+function MeetupDetail() {
+  return (
+    <Fragment>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+        alt="alter"
+      />
+      <h1>First Meetup</h1>
+      <address>Street 5 , City</address>
+      <p>Description</p>
+    </Fragment>
+  );
+}
+export default MeetupDetail;
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,10 +1,48 @@
 import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails() {
   return (
-    <Fragment>
-      <img src="" alt="" />
-    </Fragment>
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="first"
+      adress="some ity"
+      description="Firts meet"
+    />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: "First",
+        addres: "Some City",
+        description: "Firts Meet",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
